e855deeb3bc653ba60f5864536028ddef8537d3b
Otho Teixeira <otho.tk@hotmail.com>
2019-11-13 01:20:51 -0300

    Criação da função de codificação e decodificação

--- src/physical_layer.cpp
+++ src/physical_layer.cpp
@@ -19,15 +19,15 @@ void CamadaFisicaTransmissora (int quadro[]) {
   int tipoDeCodificacao = 0; //alterar de acordo o teste
   int* fluxoBrutoDeBits; //ATENÇÃO: trabalhar com BITS!!!
   switch (tipoDeCodificacao) {
-  case 0 : //codificao binaria
+  case COD_BIN : //codificao binaria
     fluxoBrutoDeBits = 
     CamadaFisicaTransmissoraCodificacaoBinaria(quadro);
     break;
-  case 1 : //codificacao manchester
+  case COD_MANCH : //codificacao manchester
     fluxoBrutoDeBits = 
     CamadaFisicaTransmissoraCodificacaoManchester(quadro);
     break;
-  case 2 : //codificacao manchester diferencial
+  case COD_MANCH_DIF : //codificacao manchester diferencial
     fluxoBrutoDeBits = 
     CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(quadro);
     break;
@@ -46,8 +46,46 @@ int* CamadaFisicaTransmissoraCodificacaoManchester (int quadro []) {
   //implementacao do algoritmo
 }//fim do metodo CamadaFisicaTransmissoraCodificacaoManchester
 
-int* CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(int quadro []){
-  //implementacao do algoritmo
+std::vector<int> CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(std::vector<int> quadro){
+  // Quadro codificado em mancheste diferencial
+  std::vector<int> quadro_manchester_diferencial;
+
+  // Bits from manchester code
+  int first_bit = 1;
+  int second_bit = 0;
+
+  // Bit de iteração do quadro codificado
+  std::vector<int>::iterator bit = quadro_manchester_diferencial.begin()++;
+
+  // Começa com 1 e 0
+  quadro_manchester_diferencial.push_back(first_bit);
+  quadro_manchester_diferencial.push_back(second_bit);
+
+  // Percorre o Quadro original
+  // Começa a codificar a partir do segundo bit, 
+  // o primeiro bit por padrão é "0 1"
+  for(std::vector<int>::iterator it=quadro.begin()+1;
+     it<quadro.end(); ++it) {
+
+    // Se for um, não há transição nos intervalos entre bits
+    // Se for zero, deve haver a transição. 
+    // Logo, não inverte o padrão do par de bits.
+    if(*it == 1) {
+      first_bit ^= 0x1;
+      second_bit ^= 0x1;
+    }
+
+    // Se for diferente de 0 ou 1, reportar erro
+    else if(*it != 0) {
+     std::cout << "\033[1;31m\ERROR! Bit inválido em Codificação Manchester\033[0m\n";
+    }
+
+    // O código Manchester possui o dobro de bits!
+    quadro_manchester_diferencial.push_back(first_bit);
+    quadro_manchester_diferencial.push_back(second_bit);
+	}
+
+  return quadro_manchester_diferencial;
 }//fim do CamadaFisicaTransmissoraCodificacaoManchesterDiferencial
 
 /* Este metodo simula a transmissao da informacao no meio de
@@ -106,7 +144,52 @@ int* CamadaFisicaReceptoraCodificacaoManchester (int quadro []) {
   //implementacao do algoritmo para DECODIFICAR
 }//fim do metodo CamadaFisicaReceptoraDecodificacaoManchester
 
-int* CamadaFisicaReceptoraCodificacaoManchesterDiferencial(int quadro[]){
+std::vector<int> CamadaFisicaReceptoraCodificacaoManchesterDiferencial(std::vector<int> quadro){
+  std::vector<int> quadro_manchester_diferencial_decodificado;
+
+  // Bits from manchester code
+  int first_bit = 1;
+  int second_bit = 0;
+  int previous_first_bit = 1;
+  int previous_second_bit = 0;
+  int invert_first_bit;
+  int invert_second_bit;
+
+  // Primeiro bit por padrão é 1
+  quadro_manchester_diferencial_decodificado.push_back(1);
+
+  // Percorre o Quadro Codificado
+  // Os dois primeiros bits são sempre 0 e 1,
+  // que representa 1 na decodificação
+  for(std::vector<int>::iterator it=quadro.begin()+2;
+     it<quadro.end(); it += 2) {
+
+    first_bit = *it;
+    it++;
+    second_bit = *it;
+    it--;
+
+    invert_first_bit  = (previous_first_bit)^(0x1);
+    invert_second_bit = (previous_second_bit)^(0x1);
+
+    if(first_bit != invert_first_bit && second_bit != invert_second_bit){
+      quadro_manchester_diferencial_decodificado.push_back(0);
+    }
+
+    else if(first_bit == invert_first_bit && second_bit == invert_second_bit){
+      quadro_manchester_diferencial_decodificado.push_back(1);
+    }
+
+    else{
+      std::cout << "\033[1;31m\ERROR! Bit inválido em Codificação Manchester\033[0m\n";
+    }
+
+    previous_first_bit = first_bit;
+    previous_second_bit = second_bit;
+  }
+
+  return quadro_manchester_diferencial_decodificado;
+
   //implementacao do algoritmo para DECODIFICAR
 }//fim do CamadaFisicaReceptoraDecodificacaoManchesterDiferencial
 
